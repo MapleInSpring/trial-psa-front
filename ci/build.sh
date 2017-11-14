5ad50c939490483a71fbc33754d85a425b028515
Yifeng Hou <yhou@pivotal.io>
2017-11-14 14:28:21 +0800

    Add Staticfile to root folder
    Signed-off-by: Yaofeng Wang <wangyf@globalpsa.com>

--- ci/build.sh
+++ ci/build.sh
@@ -8,12 +8,7 @@ npm install
 
 node_modules/@angular/cli/bin/ng build -prod --aot
 
-touch dist/Staticfile
-
-echo "pushstate: enabled" >> dist/Staticfile
-
 cp -a dist ../
 
 ls ../dist
 
-cat ../dist/Staticfile
