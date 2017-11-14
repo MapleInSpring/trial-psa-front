aa17ba0e452d334f6e6e3f2df6bea514da8fe2b3
Yaofeng Wang <wangyf@globalpsa.com>
2017-11-14 10:11:20 +0800

    Fix copy dist folder
    Signed-off-by: Yifeng Hou <yhou@pivotal.io>

--- ci/build.sh
+++ ci/build.sh
@@ -6,14 +6,12 @@ cd trial-psa-front/
 
 npm install
 
-ls node_modules/@angular/cli/bin
-
 node_modules/@angular/cli/bin/ng build -prod --aot
 
 touch dist/Staticfile
 
 echo "pushstate: enabled" >> dist/Staticfile
 
-cp dist ../dist/.
+cp -a dist ../dist
 
 ls ../dist
